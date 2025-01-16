be0f923c08e58f0936876e1e69e45c02c1de8a97
ajayjb <ajayjb11@gmail.com>
2025-01-16 23:43:02 +0530

    Feat: TS Generics

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ type Name = string;
 
 interface Person {
   name: Name;
-  age: number;
+  age: number | null | undefined | boolean;
 }
 
 const ajay: Person = {
@@ -16,9 +16,31 @@ const ajay: Person = {
   age: 10,
 };
 
-const getNumber: (num: number) => number = (num: number) => {
+const users: Person[] = [
+  {
+    name: "Ajay",
+    age: 10,
+  },
+];
+
+const getNumberArrowFunc: (num: number) => number = (num: number) => {
   return 5;
 };
+
+function getNumber(num: number): number {
+  return num;
+}
+
+// TS Generics
+function convertToArray<Type>(value: Type): Type[] {
+  return [value];
+}
+
+const convertToArrayArrowFunc = <T>(value: T): T[] => {
+  return [value];
+};
+
+convertToArray("kill");
 
 const x = getNumber(19);
 
